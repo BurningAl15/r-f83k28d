ece5b58a9c98927cbd9afdf5c213dd4212bb5627
Aldhair Vera <aldhairvera15@gmail.com>
2022-01-04 15:27:17 -0500

    feat: counting

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0
+    }
+  }
+
+  add = () => {
+    this.setState((prevState) => { return { counter: prevState.counter + 1 } })
+  }
+
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.counter}</span>
+        <button id="inc" onClick={() => { this.add() }}>Incrementa</button>
       </div>
     );
   }
